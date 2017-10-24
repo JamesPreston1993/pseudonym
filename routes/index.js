54d3bd3bb5a2df4a5280e569ec8d7f0d98a51aac
JamesPreston1993 <jamescpreston93@gmail.com>
2017-10-24 20:36:31 +0100

    Add basic functionality to send a message

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
 var router = require('express').Router();
 var request = require('request');
+var botToken = require('../config.json').botToken;
 
 router.get('/', function (req, res) {
     res.render('index');
 });
 
 router.post('/', function (req, res) {
-    
+    request.post('https://slack.com/api/chat.postMessage', {
+        form: {
+            token: botToken,
+            as_user: false,
+            username: req.body.username,
+            channel: req.body.channel,
+            text: req.body.message
+        }
+    }, function (err, response, body) {
+        res.end();
+    });
 });
 
 module.exports = router;
